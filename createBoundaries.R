8201a087e35d237115ce2cc059b2540daf104c5a
Samuel Robinson <samuel.vj.robinson@gmail.com>
2021-04-09 17:22:17 -0600

    Updated function to create boundaries

--- createBoundaries.R
+++ createBoundaries.R
@@ -13,11 +13,15 @@ datSource <- data.frame(path=dir(rootPath,pattern=".csv",recursive=TRUE)) %>%
 # datFiles <- dir('./Figures/ModelCheck/',pattern='.Rdata',full.names=TRUE)
 
 #Function to get boundary from ith field
-makeBoundary <- function(i,dS,rP,nSubSamp=50000){ 
-  # i <- 3 #Debugging
+makeBoundary <- function(i,dS,rP,outerOnly=TRUE,overwrite=FALSE,nSubSamp=50000){ 
+  # #Test
+  # # i <- 1 #Debugging
+  # i <- 2 #Debugging #2 = 2-part field
   # dS <- datSource
   # rP <- rootPath
   # nSubSamp <- 50000
+  # outerOnly <- TRUE
+  
   require(tidyverse)
   require(sf)
   
@@ -25,11 +29,11 @@ makeBoundary <- function(i,dS,rP,nSubSamp=50000){
   
   csvPath <- paste(rP,dS$path[i],sep='/')
   fieldName <- with(dS[i,],paste(grower,year,field))
-  if(dS$completed[i]){ #If shapefile already exists
+  if(dS$completed[i] & !overwrite){ #If shapefile already exists and overwrite=FALSE
     return('File already exists')
   }
   
-  print('Reading in data')
+  cat('Reading in data.')
   dat <- read.csv(csvPath,stringsAsFactors=TRUE,fileEncoding='latin1') 
   
   #Takes 40 seconds with subsamp of 50000 using full 800000 samples from Alvin French's Al Jr Field
@@ -59,18 +63,36 @@ makeBoundary <- function(i,dS,rP,nSubSamp=50000){
     mutate(E=st_coordinates(.)[,1],N=st_coordinates(.)[,2]) %>% 
     mutate(E=E-mean(E),N=N-mean(N)) #Center coordinates
   
-  print('Calculating field edge')
-  fieldEdge <- dat %>% st_union() %>% st_buffer(dist=10) %>% st_cast('MULTILINESTRING') 
+  cat(' Calculating field edge.')
+  #Get all boundaries (including internal holes)
+  fieldEdge <- dat %>% st_union() %>% 
+    st_buffer(dist=10) %>% st_buffer(dist=-5) #Dilate by 10 then erode by 5
   
-  st_write(fieldEdge,paste0('./Figures/FieldBoundaries/',fieldName,' boundary.shp'),append=FALSE)
+  if(outerOnly){ #Gets only outer boundary
+    if(st_geometry_type(fieldEdge)=='POLYGON'){
+      fieldEdge2 <- lapply(fieldEdge, function(x) st_polygon(x[1]))
+    } else if(st_geometry_type(fieldEdge)=='MULTIPOLYGON'){
+      fieldEdge2 <- lapply(fieldEdge, function(x) lapply(x,function(y) st_polygon(y[1])))
+      fieldEdge2 <- fieldEdge2[[1]] %>% do.call('st_union',.) #Union of polygons
+    } else {
+      stop('Polygon structure error')
+    }
+    fieldEdge2 <- st_sfc(fieldEdge2,crs=st_crs(fieldEdge)) #Turn into sfc
+  } else { #Gets all boundaries (including internal)
+    fieldEdge2 <- fieldEdge
+  }
   
-  gc()
+  #Write to shapefile
+  cat('. Writing to shapefile.')
+  st_write(fieldEdge2,paste0('./Figures/FieldBoundaries/',fieldName,' boundary.shp'),append=FALSE,driver='ESRI Shapefile')
+  rm(dat,fieldEdge,fieldEdge2); gc() #Garbage collection
 }
-# makeBoundary(6,datSource,rootPath)
+
+# makeBoundary(6,datSource,rootPath,overwrite=TRUE) #Test
 
 library(parallel)
 library(beepr)
 cluster <- makeCluster(8) #8 procs max
-parLapply(cl=cluster,1:nrow(datSource),makeBoundary,dS=datSource,rP=rootPath) 
+parLapply(cl=cluster,1:nrow(datSource),makeBoundary,dS=datSource,rP=rootPath,outerOnly=TRUE) 
 beep(1)
 stopCluster(cluster)
