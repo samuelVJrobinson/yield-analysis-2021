d8e32dac15a3870f87a36eb184bdf4c448fc4d52
Samuel Robinson <samuel.vj.robinson@gmail.com>
2021-06-05 17:30:57 -0600

    Updated penalization
    - For some reason, using extra shrinkage model matrix  works, but "standard" one needs another lambda term multiplied in

--- gam_TMB/gam1.cpp
+++ gam_TMB/gam1.cpp
@@ -13,6 +13,7 @@ Type objective_function<Type>::operator() (){
   DATA_SPARSE_MATRIX(S1);       //Penalty matrix - must be sparse for GMRF to work
   DATA_INTEGER(S1dim);          //Dimensions of S1
   DATA_MATRIX(newX);            //New design matrix for predictions
+  DATA_INTEGER(original);       //Use original penalty (1) or other penalty (0)
   
   //Read parameters from R------------
   PARAMETER(b0);       //Intercept
@@ -30,11 +31,24 @@ Type objective_function<Type>::operator() (){
   // Penalization part ------------------------------------
   
   //logdet requires a matrix type, and GMRF needs a sparse matrix
-  // SparseMatrix<Type> S = lambda*S1; //Penalty term * Penalty matrix
-  // nll -= 0.5*(logdet(matrix<Type>(S))) - 0.5*(GMRF(S).Quadform(smoothCoefs)); //Returns NaNs. I think this is because logDet(S) is infinite (S = rank deficient?)
+  SparseMatrix<Type> S = lambda*S1; //Penalty term * Penalty matrix
+
+  //This version 
+  //Returns NaNs. I think this is because logDet(S) is infinite (S = rank deficient?)
+  // nll -= 0.5*(logdet(matrix<Type>(S))) - 0.5*(GMRF(S).Quadform(smoothCoefs)); 
   
-  //This approach avoids logdet calculation - results are identical as far as I can tell
-  nll -= Type(0.5)*S1dim*log_lambda - 0.5*lambda*GMRF(S1).Quadform(smoothCoefs); 
+  //This approach avoids logdet calculation
+  
+  //This version is *supposed* to work, but never converges. Hasn't lambda already been multiplied by S1 in S?
+  // nll -= 0.5*S1dim*log_lambda - 0.5*lambda*GMRF(S1).Quadform(smoothCoefs);
+  
+  if(original == 0){
+    // The one that works with bs = 'tp'/'cr'
+    nll -= 0.5*S1dim*log_lambda - 0.5*lambda*GMRF(S).Quadform(smoothCoefs); 
+  } else {
+    // The one that works with bs = 'ts'/'cs': extra shrinkage
+    nll -= 0.5*S1dim*log_lambda - 0.5*GMRF(S).Quadform(smoothCoefs);  
+  }
   
   vector<Type> mu = b0 + X*smoothCoefs; //Expected value: mu = Intercept + model matrix %*% coefs
   
@@ -43,7 +57,7 @@ Type objective_function<Type>::operator() (){
   //   nll -= dnorm(Y(i), mu(i), sigma, true); 
   // }
 
-  vector<Type> splineForReport = b0 + newX*smoothCoefs; //Generate predictions 
+  vector<Type> splineForReport = b0 + newX*smoothCoefs; //Generate predictions
   ADREPORT(splineForReport); //Predicted smoother values
   
   // ADREPORT(b0); //Intercept
--- gam_TMB/gamTests1.R
+++ gam_TMB/gamTests1.R
@@ -1,5 +1,6 @@
 #GAM testing using TMB
-#Thin plate spline  - single smoother 
+
+
 
 setwd("~/Documents/yield-analysis-2021/gam_TMB")
 
@@ -7,40 +8,51 @@ library(Matrix) #Use sparse matrices
 library(TMB)
 library(mgcv)
 library(tidyverse)
+library(beepr)
 theme_set(theme_classic())
 
 library(gamair)
 data(engine)
 
-ggplot(data=engine,aes(x=size,y=wear))+geom_point()+
-  geom_smooth(method='gam',formula=y~s(x,k=7))
+#Thin plate spline  - single smoother  ---------------
 
-s0 <- gam(wear~s(size,k=8,bs='tp'),data=engine,fit=FALSE) #Problems start to occur if k<=6; Hessian non-positive definite because lambda gradient = 0
+s0 <- gam(wear~s(size,k=8,bs='ts'),data=engine,fit=FALSE) #Problems start to occur if k<=6; Hessian non-positive definite because lambda gradient = 0
+# s00 <- gam(wear~s(size,k=8,bs='ts'),data=engine,fit=FALSE)
 
 #Input
 modMat <- s0$X[,-1] #Model matrix (no intercept)
 penMat <- s0$smooth[[1]]$S[[1]] #Penalty matrix (sparse)
 penMat <- as(penMat,'sparseMatrix')
 penDim <- nrow(penMat) #Dimensions of penalty matrix
+# 
+# s0$X[,-1]-s00$X[,-1] #No difference in model matrices b/w tp and ts
+# s0$smooth[[1]]$S[[1]]-s00$smooth[[1]]$S[[1]]
+# 
+# s0$smooth[[1]]
+# s00$smooth[[1]]
+
 
 #For making predictions:
 predSize <- with(engine,seq(min(size),max(size),length.out=50)) #Size of engines to predict for
 predModMat <- PredictMat(s0$smooth[[1]],data = data.frame(size=predSize)) #New model matrix to predict with (no intercept)
 
 #Input for model
-datList <- list(Y=engine$wear, X=modMat, S1=penMat, S1dim=penDim, newX=predModMat) #Data
-parList <- list(b0=0, smoothCoefs=rep(0,datList$S1dim), log_lambda=0, log_sigma=0) #Parameters
+datList <- list(Y=engine$wear, X=modMat, S1=penMat, S1dim=penDim, newX=predModMat, original=1) #Data
+parList <- list(b0=0, smoothCoefs=rep(0,ncol(modMat)), log_lambda=0, log_sigma=0) #Parameters
 
 compile("gam1.cpp")
+beep(1)
 dyn.load(dynlib("gam1"))
 obj <- MakeADFun(data = datList, parameters = parList, random=c('b0','smoothCoefs'), DLL = "gam1")
 
 obj$par
 obj$fn() #Works
-opt <- nlminb(obj$par,obj$fn,obj$gr)
+opt <- nlminb(obj$par,obj$fn,obj$gr) #Runs into NaNs
 rep <- sdreport(obj) #Estimates and SEs
 rep #Looks OK
 rep$value
+rep$par.fixed
+rep$par.random
 
 data.frame(name=names(obj$par), Est=opt$par, final_gradient=as.vector(obj$gr(opt$par)))
 
@@ -65,7 +77,70 @@ gamPred <- predict(s1,newdata=data.frame(size=predSize),se.fit=TRUE) %>% bind_co
 tmbPred <- data.frame(fit=rep$value,se.fit=rep$sd) %>% 
   mutate(upr=fit+1.96*se.fit,lwr=fit-1.96*se.fit)
 
-bind_rows(gamPred,tmbPred) %>% mutate(mod=rep(c('GAM','TMB'),c(nrow(gamPred),nrow(tmbPred)))) %>% 
+(p <- bind_rows(gamPred,tmbPred) %>% mutate(mod=rep(c('GAM','TMB'),c(nrow(gamPred),nrow(tmbPred)))) %>% 
+    mutate(size=rep(predSize,2)) %>% 
+    ggplot(aes(x=size,y=fit))+
+    geom_point(data=engine,aes(x=size,y=wear))+
+    geom_ribbon(aes(ymax=upr,ymin=lwr,fill=mod),alpha=0.3)+
+    geom_line(aes(col=mod),size=1)+
+    scale_fill_manual(values=c('red','blue'))+
+    scale_colour_manual(values=c('red','blue'))+
+    labs(x='Size',y='Wear',col='Model',fill='Model'))
+ggsave('./gamTMB_tp.png',p)
+
+# Cubic regression spline - as above ---------------
+
+s0 <- gam(wear~s(size,k=8,bs='cr'),data=engine,fit=FALSE) 
+
+#Input
+modMat <- s0$X[,-1] #Model matrix (no intercept)
+penMat <- s0$smooth[[1]]$S[[1]] #Penalty matrix (sparse)
+penMat <- as(penMat,'sparseMatrix')
+penDim <- nrow(penMat) #Dimensions of penalty matrix
+
+#For making predictions:
+predSize <- with(engine,seq(min(size),max(size),length.out=50)) #Size of engines to predict for
+predModMat <- PredictMat(s0$smooth[[1]],data = data.frame(size=predSize)) #New model matrix to predict with (no intercept)
+
+#Input for model
+datList <- list(Y=engine$wear, X=modMat, S1=penMat, S1dim=penDim, newX=predModMat) #Data
+parList <- list(b0=0, smoothCoefs=rep(0,ncol(modMat)), log_lambda=0, log_sigma=0) #Parameters
+
+compile("gam1.cpp")
+beep(1)
+dyn.load(dynlib("gam1"))
+obj <- MakeADFun(data = datList, parameters = parList, random=c('b0','smoothCoefs'), DLL = "gam1")
+
+obj$par
+obj$fn() #Works
+opt <- nlminb(obj$par,obj$fn,obj$gr) #Runs into NaNs
+rep <- sdreport(obj) #Estimates and SEs
+rep #Looks OK
+rep$value
+rep$par.fixed
+rep$par.random
+
+#Compare to GAM fit
+s1 <- gam(G=s0,method='REML') #Get s0 and actually fit it this time
+summary(s1)
+
+#Intercepts are identical, smoothing coefs are not
+m1Tmb <- rbind(with(rep,data.frame(coef=names(par.fixed),est=par.fixed,sd=sqrt(diag(cov.fixed)),row.names=NULL)),
+               with(rep,data.frame(coef=names(par.random),est=par.random,sd=sqrt(diag.cov.random))))
+
+data.frame(gamCoefs=coef(s1),tmbCoefs=m1Tmb$est[grepl('(b0|smoothCoefs)',m1Tmb$coef)]) %>% 
+  rownames_to_column(var = 'coef') %>% mutate(coef=ifelse(grepl('Intercept',coef),'Intercept','Smoother\nCoefs')) %>% 
+  ggplot()+geom_abline(intercept = 0, slope=1)+
+  geom_point(aes(x=gamCoefs,y=tmbCoefs,col=coef))+
+  scale_colour_manual(values=c('red','black'))+
+  labs(x='GAM Coefs',y='TMB Coefs')
+
+gamPred <- predict(s1,newdata=data.frame(size=predSize),se.fit=TRUE) %>% bind_cols() %>% 
+  mutate(upr=fit+1.96*se.fit,lwr=fit-1.96*se.fit)
+tmbPred <- data.frame(fit=rep$value,se.fit=rep$sd) %>% 
+  mutate(upr=fit+1.96*se.fit,lwr=fit-1.96*se.fit)
+
+(p <- bind_rows(gamPred,tmbPred) %>% mutate(mod=rep(c('GAM','TMB'),c(nrow(gamPred),nrow(tmbPred)))) %>% 
   mutate(size=rep(predSize,2)) %>% 
   ggplot(aes(x=size,y=fit))+
   geom_point(data=engine,aes(x=size,y=wear))+
@@ -73,4 +148,5 @@ bind_rows(gamPred,tmbPred) %>% mutate(mod=rep(c('GAM','TMB'),c(nrow(gamPred),nro
   geom_line(aes(col=mod),size=1)+
   scale_fill_manual(values=c('red','blue'))+
   scale_colour_manual(values=c('red','blue'))+
-  labs(x='Size',y='Wear',col='Model',fill='Model')
+  labs(x='Size',y='Wear',col='Model',fill='Model'))
+ggsave('./gamTMB_cr.png',p)
